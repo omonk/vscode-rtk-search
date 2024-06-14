1e29649ceb49f21c182a9ed6bdda320bebb86b4e
Ollie Monk <ollie.monk@starlingbank.com>
2024-06-14 20:46:51 +0100

    Add method to find hook

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "rtk-hook-search.find-endpoint-from-hook",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rtk-hook-search" is now active!');
+      if (!editor) {
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('rtk-hook-search.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from rtk-hook-search!');
-	});
+      const runSearch = async (count = 0): Promise<unknown> => {
+        const document = editor.document;
+        await vscode.commands.executeCommand(
+          "editor.action.smartSelect.expand"
+        );
 
-	context.subscriptions.push(disposable);
+        const text = document.getText(editor.selection);
+        const reg = new RegExp(/(use)(\w+)(Query|Mutation)/);
+
+        const match = text.match(reg);
+
+        if (match != null) {
+          const [, , hookName] = match;
+
+          return vscode.commands.executeCommand("editor.actions.findWithArgs", {
+            searchString: hookName,
+            isCaseSensitive: false,
+          });
+        }
+
+        if (count < 3) {
+          return runSearch(count++);
+        } else {
+          return vscode.window.showErrorMessage("Can't find the hook, sorry");
+        }
+      };
+      await runSearch();
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
